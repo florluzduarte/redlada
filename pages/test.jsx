9d2df5d852ca3eba21cf7c2a69077f3f1eed7bae
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-03-10 16:26:59 -0300

    Add members (foundational and core) + react-circle-flag

---
+++ pages/test.jsx
@@ -0,0 +1,18 @@
+import { CircleFlag } from "react-circle-flags";
+import members from "../data/members.json";
+
+export default function TestPage() {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            {
+                members.core.map((member) => (
+                    <div>
+                        <h2>{member.name}</h2>
+                        <CircleFlag countryCode={member.flag.code} width={48} height={48} alt="Bandera argentina" />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
