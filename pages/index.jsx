db93e8c348e721d928261d98605531e6c9bf083d
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-05-08 17:41:53 -0300

    Add Objetivos component to Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
-import Valores from "@/components/home/Valores";
+// Components
 import Layout from "@/components/layout/Layout";
+import Valores from "@/components/home/Valores";
+import Objetivos from "@/components/home/Objetivos";
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home</h1>
+      <Objetivos />
       <Valores />
     </Layout>
   );
