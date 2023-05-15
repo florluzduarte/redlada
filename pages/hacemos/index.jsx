1dfd924d38fd0d2fce00b5ed43aef4969361a838
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-05-15 16:44:14 -0300

    Add Hacemos page + Add EncuentrosText

---
+++ pages/hacemos/index.jsx
@@ -0,0 +1,14 @@
+//Components
+import Layout from "@/components/layout/Layout";
+import EncuentrosText from "@/components/hacemos/EncuentrosText";
+
+const Hacemos = () => {
+  return (
+    <Layout>
+      <h1>Hacemos</h1>
+      <EncuentrosText />
+    </Layout>
+  );
+};
+
+export default Hacemos;
