f3177c94f3d0663c91d3b94d87df8ef351619d7d
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-06-06 11:24:28 -0300

    Add Header general component

---
+++ components/general/Header.jsx
@@ -0,0 +1,27 @@
+//Components
+import Button from "./Button.jsx";
+
+//Styles
+import styles from "../../styles/general/Header.module.css";
+
+const Header = ({ titleSection, btn }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <h1 className={`${styles.header__title} ${btn.color}`}>
+          {titleSection}
+        </h1>
+        <div className={styles.header__button}>
+          <Button
+            title={btn.titleBtn}
+            href={btn.href}
+            icon={btn.icon}
+            color={btn.color}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/somos/fundacional.jsx
+++ pages/somos/fundacional.jsx
@@ -1,8 +1,25 @@
+//Components
+import Layout from "@/components/layout/Layout";
+import Header from "@/components/general/Header";
+
+const propsHeader = {
+  titleSection: "Grupo fundacional",
+  btn: {
+    titleBtn: "Volver a Somos",
+    href: "/somos",
+    icon: {
+      position: "left",
+      type: "regular",
+    },
+    color: "yellow",
+  },
+};
+
 const Fundacional = () => {
   return (
-    <>
-      <h1>Grupo Fundacional</h1>
-    </>
+    <Layout>
+      <Header titleSection={propsHeader.titleSection} btn={propsHeader.btn} />
+    </Layout>
   );
 };
 
