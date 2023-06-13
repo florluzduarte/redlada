a4eee7570dc882911b308347af8213a4192ccb24
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-06-13 12:17:07 -0300

    Add CardsFlag and Update CardCountryFlag, Finish index nodos

--- components/somos/nodos/CardCountryFlag.jsx
+++ components/somos/nodos/CardCountryFlag.jsx
@@ -1,9 +1,32 @@
-const CardCountryFlag = () => {
+//Libraries
+import Link from "next/link";
+import { CircleFlag } from "react-circle-flags";
+import PropTypes from "prop-types";
+
+//Styles
+import styles from "../../../styles/somos/nodos/CardCountryFlag.module.css";
+
+const CardCountryFlag = ({ link, title, flag }) => {
   return (
-    <>
-      <p>Country Flag Nodos</p>
-    </>
+    <Link href={link}>
+      <div className={styles.card__data__wrapper}>
+        <CircleFlag
+          countryCode={flag.code}
+          alt={flag.alt}
+          width={112}
+          height={112}
+          className={styles.card__flag}
+        />
+        <p>{title}</p>
+      </div>
+    </Link>
   );
 };
 
 export default CardCountryFlag;
+
+CardCountryFlag.propTypes = {
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  flag: PropTypes.object.isRequired,
+};
--- pages/somos/nodos/index.jsx
+++ pages/somos/nodos/index.jsx
@@ -2,7 +2,7 @@
 import Layout from "../../../components/layout/Layout";
 import Header from "../../../components/general/Header";
 import NavigationMembers from "@/components/general/NavigationMembers";
-import CardCountryFlag from "@/components/somos/nodos/CardCountryFlag";
+import CardsFlag from "@/components/somos/nodos/CardsFlag";
 
 const propsHeader = {
   titleSection: "Nodos",
@@ -21,7 +21,7 @@ const Nodos = () => {
   return (
     <Layout>
       <Header btn={propsHeader.btn} titleSection={propsHeader.titleSection} />
-      <CardCountryFlag />
+      <CardsFlag />
       <NavigationMembers btn={propsHeader.btn} />
     </Layout>
   );
