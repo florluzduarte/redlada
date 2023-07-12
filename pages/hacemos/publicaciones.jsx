3453471abf3f4b4c847f65f67866b0677de6258e
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-07-12 15:50:14 -0300

    Finish publicaciones page, add PublicacionesPapers component

--- pages/hacemos/publicaciones.jsx
+++ pages/hacemos/publicaciones.jsx
@@ -2,7 +2,7 @@
 import Layout from "@/components/layout/Layout";
 import Header from "@/components/general/Header";
 import PublicacionesDescription from "@/components/hacemos/PublicacionesDescription";
-import CardPublicaciones from "@/components/hacemos/CardPublicaciones";
+import PublicacionesPapers from "@/components/hacemos/PublicacionesPapers";
 import NavigationMembers from "@/components/general/NavigationMembers";
 
 //Data
@@ -21,23 +21,12 @@ const propsHeader = {
   },
 };
 
-const card = {
-  title:
-    "Memorias del 9no Encuentro Latinoamericano de Food Design. Replantar.",
-  href: "https://drive.google.com/file/d/1gm9p8IuoSjjrlpfmpEg8tMBEzoXXiR-x/view?usp=sharing",
-  date: "Julio 2021",
-  author: "Red Latinoamericana de Diseño y Alimentos",
-  isbn: "978-9974-8752-2-7",
-  color: "orange",
-  bkg: "bkg__orange",
-};
-
 const Publicaciones = () => {
   return (
     <Layout>
       <Header titleSection={propsHeader.titleSection} btn={propsHeader.btn} />
       <PublicacionesDescription data={publicacionesDescription} />
-      <CardPublicaciones cardData={card} />
+      <PublicacionesPapers />
       <NavigationMembers btn={propsHeader.btn} />
     </Layout>
   );
